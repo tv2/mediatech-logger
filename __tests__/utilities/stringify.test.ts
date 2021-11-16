249ddbaf128510d4aff310b25bbdf66886a805f8
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-11-16 16:09:47 +0100

    fix: Added stringify to utilities.

---
+++ __tests__/utilities/stringify.test.ts
@@ -0,0 +1,110 @@
+import { stringify } from '../../src/utilities'
+
+test('primitives', () => {
+  expect(stringify(1)).toBe('1')
+  expect(stringify(-1)).toBe('-1')
+  expect(stringify(1.525)).toBe('1.525')
+  expect(stringify(-1.525)).toBe('-1.525')
+  expect(stringify(1n)).toBe('1')
+  expect(stringify(-1n)).toBe('-1')
+  expect(stringify(true)).toBe('true')
+  expect(stringify(false)).toBe('false')
+  expect(stringify(null)).toBe('null')
+  expect(stringify(undefined)).toBe('undefined')
+  expect(stringify('true')).toBe('"true"')
+  expect(stringify('false')).toBe('"false"')
+  expect(stringify('null')).toBe('"null"')
+  expect(stringify('undefined')).toBe('"undefined"')
+  expect(stringify('hello world!')).toBe('"hello world!"')
+  expect(stringify('')).toBe('""')
+})
+
+test('arrays', () => {
+  expect(stringify([])).toBe('[]')
+  expect(stringify([1, 2, 3, 4, 5])).toBe('[1,2,3,4,5]')
+  expect(stringify([1.2, 2.3, 3.4, 4.5, 5.6])).toBe('[1.2,2.3,3.4,4.5,5.6]')
+  expect(stringify(['1.2', '2.3', '3.4', '4.5', '5.6'])).toBe('["1.2","2.3","3.4","4.5","5.6"]')
+})
+
+test('objects', () => {
+  expect(stringify({})).toBe('{}')
+  expect(
+    stringify({
+      integer: 1,
+      float: 1.123,
+      bigint: 32n,
+      emptyString: '',
+      string: 'hello there',
+      false: false,
+      true: true,
+      null: null,
+      undefined: undefined,
+    })
+  ).toBe(
+    '{"integer":1,"float":1.123,"bigint":32,"emptyString":"","string":"hello there","false":false,"true":true,"null":null,"undefined":undefined}'
+  )
+})
+
+test('misc', () => {
+  expect(
+    stringify({
+      attr1: [1, 2, 3, 4],
+    })
+  ).toBe('{"attr1":[1,2,3,4]}')
+  expect(
+    stringify({
+      attr0: "value0",
+      attr1: [1, 2, 3, 4],
+      attr2: {
+        'attr2.1': [1],
+        'attr2.2': null,
+      },
+    })
+  ).toBe('{"attr0":"value0","attr1":[1,2,3,4],"attr2":{"attr2.1":[1],"attr2.2":null}}')
+})
+
+test('depth 0', () => {
+  expect(stringify([], { depth: 0n })).toBe('"<max-depth-reached>"')
+})
+
+test('misc with depth 1', () => {
+  expect(
+    stringify(
+      {
+        attr1: [1, 2, 3, 4],
+      },
+      { depth: 1n }
+    )
+  ).toBe('{"attr1":"<max-depth-reached>"}')
+  expect(
+    stringify({
+      attr0: "value0",
+      attr1: [1, 2, 3, 4],
+      attr2: {
+        'attr2.1': [1],
+        'attr2.2': null,
+      },
+    }, { depth: 1n })
+  ).toBe('{"attr0":"value0","attr1":"<max-depth-reached>","attr2":"<max-depth-reached>"}')
+})
+
+test('misc with depth 2', () => {
+  expect(
+    stringify(
+      {
+        attr1: [1, 2, 3, 4],
+      },
+      { depth: 1n }
+    )
+  ).toBe('{"attr1":"<max-depth-reached>"}')
+  expect(
+    stringify({
+      attr0: "value0",
+      attr1: [1, 2, 3, 4],
+      attr2: {
+        'attr2.1': [1],
+        'attr2.2': null,
+      },
+    }, { depth: 2n })
+  ).toBe('{"attr0":"value0","attr1":[1,2,3,4],"attr2":{"attr2.1":"<max-depth-reached>","attr2.2":null}}')
+})
--- src/utilities/stringify.ts
+++ src/utilities/stringify.ts
@@ -1,4 +1,37 @@
+export interface IStringifyOptions {
+  depth?: bigint
+}
 
-export function stringify(obj: object): string {
-  return JSON.stringify(obj)
+export function stringify(item: any, options?: IStringifyOptions): string {
+  const maxDepth = options?.depth ?? -1n
+  return _stringify(item, maxDepth)
+}
+
+function _stringify(item: any, depth: bigint): string {
+  // Exit if reached max depth and is a collection
+  const isCollection = Array.isArray(item) || (typeof item === 'object' && item !== null)
+  if (depth === 0n && isCollection) {
+    return '"<max-depth-reached>"'
+  }
+  // Array
+  if (Array.isArray(item)) {
+    const content = item.map((subitem) => _stringify(subitem, depth - 1n)).join(',')
+    return `[${content}]`
+  }
+  // Object
+  if (typeof item === 'object' && item !== null) {
+    const content = Object.keys(item).map((key) => `"${key}":${_stringify(item[key], depth - 1n)}`)
+    return `{${content}}`
+  }
+  // BigInt
+  if (typeof item === 'bigint') {
+    return item.toString()
+  }
+  // String
+  if (typeof item === 'string') {
+    const content = item.replace(/"/g, '\\"')
+    return `"${content}"`
+  }
+  // Fallback
+  return String(item)
 }
