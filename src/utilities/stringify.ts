de3b41580f30870fa2be501b5eef48df1db70f60
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-11-18 11:54:12 +0100

    fix: Updated stringify for Error and options passing.

--- src/utilities/stringify.ts
+++ src/utilities/stringify.ts
@@ -1,38 +1,39 @@
-export interface IStringifyOptions {
-  depth?: bigint
+export interface StringifyOptions {
+  depth: bigint
 }
 
 /**
  * Returns a JSON string representation of any value.
  **/
-export function stringify(item: any, options?: IStringifyOptions): string {
-  const maxDepth = options?.depth ?? -1n
-  return _stringify(item, maxDepth)
+export function stringify(item: any, options: Partial<StringifyOptions> = {}): string {
+  const depth = options.depth ?? -1n
+  return _stringify(item, depth, { ...options, depth })
 }
 
 /**
  * Helper for recursive generation of JSON string.
+ * @param depthCapacity Specifies how many levels further we can go down into an object.
  **/
-function _stringify(item: any, depth: bigint): string {
+function _stringify(item: any, depthCapacity: bigint, options: StringifyOptions): string {
   // Exit if reached max depth and is a collection
   const isCollection = Array.isArray(item) || (typeof item === 'object' && item !== null)
-  if (depth === 0n && isCollection) {
+  if (depthCapacity === 0n && isCollection) {
     return '"<max-depth-reached>"'
   }
   // Error
   if (item instanceof Error) {
-    const content = item.toString().replace(/"/g, '\\"')
+    const content = (item.stack ?? item.toString()).replace(/"/g, '\\"')
     return `"${content}"`
   }
   // Array
   if (Array.isArray(item)) {
-    const content = item.map((subitem) => _stringify(subitem, depth - 1n)).join(',')
+    const content = item.map((subitem) => _stringify(subitem, depthCapacity - 1n, options)).join(',')
     return `[${content}]`
   }
   // Object
   if (typeof item === 'object' && item !== null) {
     const content = Object.keys(item)
-      .map((key) => `"${key}":${_stringify(item[key], depth - 1n)}`)
+      .map((key) => `"${key}":${_stringify(item[key], depthCapacity - 1n, options)}`)
       .join(',')
     return `{${content}}`
   }
