8dada0e566bcf49db12dfadb8c5ea66553f86180
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-11-30 15:39:54 +0100

    refactor: Use enum for log level in format test.

--- __tests__/unit/format.test.ts
+++ __tests__/unit/format.test.ts
@@ -1,25 +1,26 @@
 import { applyFormat, Format, Formatter } from '../../src/logger/format'
+import { Level } from '../../src/logger/level'
 
 test('applyFormat (Plaintext)', () => {
-    const logInfo = { level: 2, message: 'hello world!' }
+    const logInfo = { level: Level.Info, message: 'hello world!' }
     expect(applyFormat(logInfo, { kind: Format.Plaintext })).toMatch(/\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z\] \[info\]: hello world!/)
     expect(applyFormat(logInfo, { kind: Format.Plaintext, timestamp: true })).toMatch(/\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z\] \[info\]: hello world!/)
 })
 
 test('applyFormat (Plaintext + tag)', () => {
-    const logInfo = { level: 2, message: 'hello world!', tag: 'relevant-tag' }
+    const logInfo = { level: Level.Info, message: 'hello world!', tag: 'relevant-tag' }
     expect(applyFormat(logInfo, { kind: Format.Plaintext, timestamp: false })).toBe('[info] <relevant-tag>: hello world!')
     expect(applyFormat(logInfo, { kind: Format.Plaintext, timestamp: true })).toMatch(/\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z\] \[info\] <relevant-tag>: hello world!/)
 })
 
 test('applyFormat (Plaintext + color)', () => {
-    const logInfo = { level: 2, message: 'hello world!', tag: 'relevant-tag' }
+    const logInfo = { level: Level.Info, message: 'hello world!', tag: 'relevant-tag' }
     expect(applyFormat(logInfo, { kind: Format.Plaintext, color: true, timestamp: false })).toBe('[\x1b[32minfo\x1b[0m] <relevant-tag>: hello world!')
     expect(applyFormat(logInfo, { kind: Format.Plaintext, color: true, timestamp: true }).replaceAll('\x1b[32m', '\\x1b[32m').replaceAll('\x1b[0m', '\\x1b[0m').replaceAll('\x1b[4m', '\\x1b[4m')).toMatch(/\[\\x1b\[4m\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z\\x1b\[0m\] \[\\x1b\[32minfo\\x1b\[0m\] <relevant-tag>: hello world!/)
 })
 
 test('applyFormat (Json)', () => {
-    const logInfo = { level: 2, message: "test"}
+    const logInfo = { level: Level.Info, message: "test"}
     expect(applyFormat(logInfo, { kind: Format.JSON })).toMatch(/{"level":"info","message":"test","timestamp":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z"}/)
     expect(applyFormat(logInfo, { kind: Format.JSON, timestamp: false })).toBe("{\"level\":\"info\",\"message\":\"test\"}")
     expect(applyFormat(logInfo, { kind: Format.JSON, timestamp: true })).toMatch(/{"level":"info","message":"test","timestamp":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z"}/)
@@ -29,6 +30,6 @@ test('applyFormat (Custom)', () => {
     const format: Formatter = (log, options) => {
         return log.message + (log.tag ?? 'no-tag') + (options.timestamp ? Date.now() : '')
     }
-    const logInfo = { level: 2, message: "test"}
+    const logInfo = { level: Level.Info, message: "test"}
     expect(applyFormat(logInfo, { kind: Format.Custom, format})).toBe('testno-tag')
 })
